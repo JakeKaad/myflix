66b7c21ea916f9038221181e3c67a4628e45b383
Jake Kaad <kaadalac@gmail.com>
2015-01-23 13:42:52 -0800

    add shoulda-matchers

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 
 describe Video do 
-  it "saves itself" do
-    video = Video.new(title: "Gotham", description: "batman!")
-    video.save
-    expect(Video.first).to eq(video)
-  end
+ it { should belong_to(:category) }
+ it { should validate_presence_of(:title) }
+ it { should validate_presence_of(:description) }
 
 end
