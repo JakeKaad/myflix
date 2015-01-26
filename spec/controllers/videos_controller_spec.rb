67835292773b2ef19395a2c37d0e36477b211104
Jake Kaad <kaadalac@gmail.com>
2015-01-25 22:36:01 -0800

    Add search tests

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -10,9 +10,23 @@ describe VideosController do
       expect(assigns(:video)).to eq(video)
     end
 
-    it "redirects unauthenticated users to the signin page" do 
+    it "redirects unauthenticated users to the login page" do 
       video = Fabricate(:video)
       get :show, id: video.id
+      expect(response).to redirect_to login_path
+    end
+  end
+
+  describe "POST search" do
+    it "sets @results for authenticated users" do 
+      session[:user_id] = Fabricate(:user).id
+      monk = Fabricate(:video, title: "monk")
+      post :search, search_term: "monk"
+      expect(assigns(:results)).to eq([monk])
+    end
+    it "redirectes unauthenticated to login page" do 
+      video = Fabricate(:video)
+      post :search, search_term: "monk"
       expect(response).to redirect_to login_path
     end
   end
